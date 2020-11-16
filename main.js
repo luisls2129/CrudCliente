e56d1ce59806bdaa58aff1a0129cb7be313d4909
juan <luisls2129@gmail.com>
2020-11-16 11:28:14 +0100

    put patch

--- main.js
+++ main.js
@@ -73,6 +73,74 @@ function POST(){
     };
 }
 
+function PUT(){
+
+    let xhr = new XMLHttpRequest();
+
+    let id = prompt("introduzca el id a cambiar");
+
+    xhr.open("PUT","http://localhost:3000/alumnos/" + id);
+    xhr.setRequestHeader('content-type', 'application/json; charset=utf-8')
+
+    let nuevoAlum = {
+        "id" : id,
+        "nombre" : prompt("introduzca su nombre"),
+        "edad" : prompt("introduzca su edad")
+    }
+
+    nuevoAlum = JSON.stringify(nuevoAlum);
+
+    xhr.responseType = "json";
+
+    xhr.send(nuevoAlum);
+
+    xhr.onload = () =>{
+
+        if(xhr.status == 200){
+            
+            console.log("Cambiado correctamente");
+            console.log(xhr.response);
+        }else{
+            error(xhr);
+        }
+        
+    };
+
+}
+
+function PATCH(){
+
+    let xhr = new XMLHttpRequest();
+
+    let id = prompt("introduzca el id a cambiar");
+
+    xhr.open("PATCH","http://localhost:3000/alumnos/" + id);
+    xhr.setRequestHeader('content-type', 'application/json; charset=utf-8')
+
+    let nuevoAlum = {
+        "nombre" : prompt("introduzca su nombre")
+    }
+
+    nuevoAlum = JSON.stringify(nuevoAlum);
+
+    xhr.responseType = "json";
+
+    xhr.send(nuevoAlum);
+
+    xhr.onload = () =>{
+
+        if(xhr.status == 200){
+            
+            console.log("Cambiado correctamente");
+            console.log(xhr.response);
+        }else{
+            error(xhr);
+        }
+        
+    };
+
+}
+
 function error(xhr){
     console.log("ERROR en el envio de datos: " + xhr.status + " " + xhr.statusText);
 }
